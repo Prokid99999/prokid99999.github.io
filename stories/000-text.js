2c99f635e68ebd6f0da38d937ac2b1a22ef687fb
benheroblaw <benheroblade@gmail.com>
2026-01-25 21:24:06 -0600

    magic update

---
+++ stories/000-text.js
@@ -0,0 +1,195 @@
+const magic = ["<h2>1</h2> \
+<h1 class=\"noza\">Noza</h1> \
+  <div class=\"background\"> \
+    <p>I'm walking to math class with my roommates and best friends, Mina and Airn. Mina looks like she's going to get some sleep during learning time. She's been doing pretty well, getting straight A's but almost never an A+. I'm starting to think she's trying to get lower grades so she has less attention on herself. Her girlfriend's and mine seems like enough. Really, she's the least attention-hungry succubus I know. She pays attention in class and answers questions while the others rule their classes. Like, seriously, they make up at least half of the student council. They can't even do it honestly! Whenever I walk past one, I feel a prickle of magic drawing my eyes to their skimpy clothes that can't be allowed through the dress code. There's something seriously wrong with our school if they allow students to walk around in bikini tops and have half their butts out of their shorts but pester Airn about her vest.</p> \
+    <p>Speaking of her, Airn's less than thrilled. I bet she'd rather be doing magical equations than numerical, especially because she's failing. Mina and I have been doing our damndest to help, and she's been trying, but her grades have only gone from straight F's to D's. We're getting worried they're going to make her retake the grade, but it's so unfair. I've never seen someone get put on their ass quicker than Airn can. During magic sparring, students line up to see who can last the longest against her. The record is two minutes, set by Mina. The closest behind her is forty-five seconds. My personal theory is: she's hiding a bunch of magic items covered in protections. That, along with what she denies is a natural gift for spell-casting, puts her up with some of the higher-level arch wizards here. So what if it takes her a while to figure out what number <i>X</i> is? She'll have your surrender in a quarter of that time.</p> \
+    <p>\"So, what black-magic bullshit do you think Mrs. Johann has today?\" Airn asks. Both of us chuckle, ignoring whatever the crowd around us might think.</p> \
+    <p>\"It's <i>math</i>! And she's not even a mage.\" Of course Mina would know. She says it's because she's a magic-born creature, even though slimes are magic-born and I wouldn't know whether Airn was a normal human or a god taking a stroll among us mortals.</p> \
+    <p>\"She's still using dark magic! Mages use the mana in our bodies; she uses runes.\"</p> \
+    <p>\"What, can you not come up with a spell to defend yourself?\" Dammit, I gave her an idea. Her eyes unfocus and glance around at a diagram nobody else sees while muttering ingredients and possible equations. Mina nudges her and says \"Eyes on the road\" like she wouldn't guide Airn through the whole school.</p> \
+    <p>\"Aw, and we're at the worst hour of the day.\" Airn's face takes on the resigned and depressed expression she wears during math class.</p> \
+    <p>\"Come on, it's supposed to be review.\" Honestly, the only reason Airn's still taking math is because Mina's there. She would drop it immediately if her favorite person wasn't there and trying to teach her. To slightly more avail than Mrs. Johann, <i>might I add.</i> She <i>tries</i> if Mina's helping!</p> \
+    <p>Once we're all seated, Mrs. Johann says, \"Alright, class, today we'll be reviewing fractions.\" Airn sighs, apparently loud enough for the teacher to hear.</p> \
+    <p>\"Is there a problem, Miss Avalon?\"</p> \
+    <p>\"No, ma'am!\" Airn says, with an impressive fake smile.</p> \
+    <p>\"Then let's begin.\"</p> \
+    <div style=\"font-size: 150%; text-align: center;\">~~&gt ~~&gt ~~&gt</div> \
+    <p>I can't help looking at a newer student down the row from me. They're a slime, and a young one at that. Their body is more liquid than solid, and they're having trouble staying in their seat. Mrs. Johann keeps giving them looks that make me bristle and them shrink. I want to beat our teacher to death with rocks, but I'm sure that would get me unflattering wanted posters, so I settle for solidifying my body in a position that makes me look busy and send a little ball of slime down the row.</p> \
+    <p>When I get to the new slime's seat, I slide up the legs of their desk and arrange myself in a puddle on top of it. They stare at me, so I whisper, \"Hold up something to read.\"</p> \
+    <p>They root through their bag and pull out a math textbook. I slip closer to them as they set it open on their desk. Mrs. Johann looks our way. I look at the pages and quietly order them to turn it right-side-up. They oblige, and I assume my humanoid form. Scaled down to the size of my hand. And between the pages of a math book.</p> \
+    <p>\"How did you do that?\" They really <i>are</i> young. Their eyes are wide as they roam over my body, trying to make sense of it. Hopefully the teacher thinks they're really interested in math.</p> \
+    <p>\"It's pretty simple, just imagine the surface of your body is made of something hard. Like ice over a lake.\" They look at their hand and close their eyes. The thin blue of their skin turns cloudy. I poke their hand, and it feels much harder than before.</p> \
+    <p>\"Now imagine the ice thawing, so it's solid but not hard.\" Their eyes shut tighter and the ice-blue of their skin shifts closer to what it was before. I poke them again, and find it worked excellently. Their skin is exactly solid enough to move without much difficulty.</p> \
+    <p>\"Congratulations! You're not a puddle anymore!\" Aww, they're so proud of themself. They don't even need to make their eyes bioluminescent to glow.</p> \
+    <p>They shoot a worried look at my body-shaped statue. I cock my head at them; then I hear Mrs. Johann.</p> \
+    <p>\"Miss Noza? Care to answer the question?\"</p> \
+    <p>I solidify my little puppet, throw my consciousness across the room to my main body, unfreeze it, and look at the board in less than half a second. The problem she wrote isn't too hard. \"I'm sorry, what did you say?\"</p> \
+    <p>\"You need to start paying attention, young lady.\" I want to yell that I will outlive her puny human life and am fully capable of doing the problem if she tells me what is.</p> \
+    <p>\"I apologize for helping a fellow slime keep their body together. What was the question?\" Doesn't sound so good now, does it? I can feel Airn's wicked grin across the room; Mina shakes her head next to her.</p> \
+    <p>\"Excuse me?\" My pupil is giving me a pleading look. They would rather be a puddle than be in this situation.</p> \
+    <p>\"What was the question?\"</p> \
+    <p>\"Who were you <i>helping?</i>\" I gesture down the row to the slime.</p> \
+    <p>\"This kind young... I forgot to ask about your gender.\" I cock my head. \"And your name.\"</p> \
+    <p>\"M-my name is- Arna. I'm a- I'm a girl.\" She looks so pitiful. I bet the <i>goddamn glare</i> Mrs. Johann is aiming at her isn't helping either.</p> \
+    <p>\"I was helping Arna keep her shape.\" The whole class is glancing between me and the teacher like a fight is about to break out.</p> \
+    <p>\"While you were supposed to be focusing on the lesson.\" Mrs. Johann's eyes are narrowed on me. It feels like she's trying to get a confession of a crime.</p> \
+    <p>\"It's much easier to focus on math when you don't have to keep picking your body up off the floor.\" I did nothing wrong.</p> \
+    <p>\"Well, if you have so much to teach, why don't you come up here?\" She's glaring at me with vengance in her eyes. I get out of my chair and walk up to the board. Mrs. Johann takes my seat, to my annoyance. Either way, I have a class to teach.</p> \
+    <p>\"Hello, everyone! Welcome to Slime Biology.\" I use a tendril of slime to grab the eraser off the whiteboard and erase the math problem. I bet Airn would be cheering if we weren't in class.</p> \
+    <p>I write the words 'Slime Biology' on the board, and someone asks, \"How did you do that?\"</p> \
+    <p>\"Raise your hand if you have a question!\" Ms. Johann snaps. They look down.</p> \
+    <p>\"I believe I'm in charge of this class, Miss Johann. Please try to be kind, and if you don't have anything kind to say, please keep your mouth shut.\" Half the class, including Ms. Johann, is staring at me in disbelief, and the rest is staring in awe.</p> \
+    <p>\"Any more questions about the expectations in my classroom?\" I look around at the students. One boy speaks for the whole class when he asks, \"We can just... talk?\"</p> \
+    <p>\"You should give everyone who wants to speak a chance, but yes.\" I wait a second for any more questions. It's disappointing how confused they are.</p> \
+    <p>\"I believe you had a question?\" I look at the person who spoke up first. \"How I wrote this on the board?\" I say, gesturing at the words.</p> \
+    <p>\"Y-yeah...\"</p> \
+    <p>\"I put some slime on the board and changed its color.</p> \
+    <p>\"I'm sure you all know that slimes can change their shape, but how many of you know how much control we can have?\" Nobody volunteers. \"How many of you know the extent of our power? How many of you would know that I was a slime if I <i>tried</i> to look human?\" I shift my body, changing colors and shape, until I'm in my human disguise. \"How many of you can tell I'm not an average person?\"</p> \
+    <p>\"Your mana looks a lot different!\" Airn calls.</p> \
+    <p>\"Really? What's the difference?\" I focus on her, getting eager to prove to Ms. Johann that my friend is smarter than the rock she seems to think Airn is.</p> \
+    <p>\"Untrained humans have mana bouncing around all over their bodies. At the very least, you know how to control the flow of your magic.\" She shrugs. \"Maybe you're a mage, maybe you just like meditating. Who knows?\"</p> \
+    <p>\"You don't?\" Her eyes narrow, taking the bait. I have the sudden thought of Airn duelling Ms. Johann.</p> \
+    <p>\"Slimes have a particular way of manipulating their mana, just like every other magic-born.\" She leans back, slipping into a confident pose.</p> \
+    <p>\"How do I manipulate mine?\" Ms. Johann is looking at Airn with a question in her eyes. I bet she's wondering why the best mage in the school chose to learn about magic instead of math, completely missing the rather important detail that Airn <i>does</i> want to be good at math. Even if it's only so she can get to the next grade.</p> \
+    <p>\"Every cell in your body is bound to a tiny piece of mana. You take that magic and move it around, change its color, and because it's part of a cell, the cell moves and changes with it. It's a very difficult style of magic for humans to replicate- we just don't have enough control to bind mana into more than a few cells at a time.\"</p> \
+    <p>\"Look at that, even I'm learning something today,\" I say to the class.</p> \
+    <p>\"It's nothing special. Most magic-born couldn't tell you their magic style; they just understand that it's natural.\" There's her smug look. Well, maybe not <i>smug</i>, but she knows she's right and is ready to take anyone who disagrees to the sparring gym. Mina's eyes look perfectly normal even though she's staring at Airn. I bet it's taking everything in that unnaturally attractive skull to not make out with her right here.</p> \
+    <p>\"Now, if you'll excuse me, I have to use the restroom.\" Airn starts to get up, and is rudely interrupted by Ms. Johann.</p> \
+    <p>\"You need-\"</p> \
+    <p>\"You can go.\" I wave her on and pin the old bat with a glare. I would rather <i>not</i> have students pissing themselves, even though I doubt that's what Airn is going to the bathroom for.</p> \
+    <p>\"Any other questions?\"</p> \
+    <br> \
+    <p>A few minutes later, Airn hasn't come back and I notice that Mina is gone. Looks like I was right. Anyway, I answer more questions, and Airn comes back. Mina shows up too, and both of them look normal. Aside from the slight sheen of magic over them. They don't speak up for the rest of class, so I finish it up and dismiss everyone.</p> \
+    <br> \
+    <p>As we walk to the cafeteria, Mina takes off the magic hiding how disheveled the two are. It's not that bad, honestly. Mina's hair is a little messy and both of their shirts are ruffled, but that's it.</p> \
+    <p>\"You two have fun?\" I'm being cheeky, I know.</p> \
+    <p>\"Hell yeah! That was the best class I've taken here!\" She can't be lying with her eyes lit up like that, and it's not just from her alone time with Mina.</p> \
+    <p>\"But you couldn't wait fifteen minutes?\"</p> \
+    <p>\"Yeah, sorry about that.\" Mina's predicably sheepish. Airn grins and says, \"I've been told I'm irresistible.\"</p> \
+    <p>\"Well, I've got a slime to go see.\" Now Airn's looking cheeky.</p> \
+    <p>\"Did you pull her in <i>ten minutes?</i>\" I mean, I've been known to sleep around, but I need at <i>least</i> half of to one hour to get to know someone. Low standards, not <i>no</i> standards.</p> \
+    <p>\"No, just talking slime things.\" She's got a shit-eating grin now. \"And you need to go to <i>her</i> room for that?\"</p> \
+    <p>\"Nah, we're going to bond over dinner.\"</p></div> \
+    </div>",
+
+  '<h2>2</h2> \
+  <h1 class="airn">Airn</h1> \
+  <div class="background"> \
+    <p>Mina walks over to my desk. I sense her without my eyes or ears; a presence behind me combined with a unique, shifting flow of mana marks this person-shaped object as my girlfriend. I\'ve explained it to her before, probably more than once. Focusing on one person\'s mana for long enough lets you recognize patterns you wouldn\'t otherwise. It\'s like staring at someone\'s face until you memorize it. Plus, monitoring someone\'s mana is easier (and a lot less weird) than staring them down for hours at a time.</p> \
+    <p>She hugs me and sits in the chair next to mine. I want to light my homework on fire and wrap my arms around her and touch her and kiss her and fall asleep next to her.<br> \
+    Instead, I show her my math homework, covered in handwritten equations and scribbled-out swears. The school requires a 90% score in math to continue to the next grade. I barely have eighty percent after studying long past midnight for a minimum of five days a week. What have I learned? How to destroy my health and put on a mask.</p> \
+    <p>I just want to stay in the same grade as Mina. The teasing I would recieve for being held back can be fixed easily. Show the class a few spells and I\'m golden, or at least metallic. What doesn\'t make sense is why I would have to retake a grade with 110% in Magic Studies. This is a <i>magic</i> school. Why the fuck do I need to learn math? How the hell does putting numbers together make me a better mage? <b><i>It fucking doesn\'t</i></b>. Anything more than basic algerbra is unnecessary. Mana isn\'t something that can be measured. Arch-mages are arch-mages because they\'re just too damn powerful to be considered regular magic users. Why is this school forcing me to lose hours every day shoving techniques I\'ll never use into my skull? If I could use a spell to put algebra skills directly into my brain, I... Well, I wouldn\'t use it, but I would make a charm for my vest or something. <i>Allow the wearer of this charm to perform mathematical equations no matter the difficulty...</i> That kind of spell would be considered cheating. However, if I worded it to enhance the wearer\'s <i>existing</i> ability...</p> \
+    <p>"Airn?" Mina drags me out of my fantasy with a tired look. I don\'t like that she\'s losing sleep over this too... but I\'m happy she\'s next to me. I wonder if I\'m selfish for thinking that.</p> \
+    <p>"Sorry..." I don\'t care right now. I want to be done with this bullshit so I can go to bed.<br> \
+    ...Too bad I won\'t be able to enjoy cuddling with her.</p> \
+    <div style=\"font-size: 150%; text-align: center;\">~~&gt ~~&gt ~~&gt</div> \
+  </div>']
+
+const lustwreakers = ['<!DOCTYPE html>\
+<head>\
+  <link rel=\"stylesheet" href="../characters.css">\
+  <link rel="icon" href="../icons/lustwreaker.png">\
+</head>\
+<title>Marnin Lustwreaker</title>\
+<html>\
+  <body>\
+    <h1 style="color: #980000;">Marnin<span style="color: white;">(Marie)</span> Lustwreaker</h1>\
+    <p>\
+    She/Her<br>\
+    Female, but she can use the other one<br>\
+    20<br>\
+    Lesbian<br>\
+    Demonic power/magic<br>\
+    Demonoid/Cambion(Half human, half Arch Succubus)<br><br>\
+    Marnin is her demon name and Lustwreaker is her clan, nobody is related to each other except Quinn<br>\
+    Rory is second-in-command, while Diolas and Raein are only members in name and are free to do as they wish(not to say that Rory isn\'t; she simply \prefers working under Marnin)<br>Quinn will be third-in-command\
+    </p>\
+    <img src="lustwreaker.png">\
+  </body>\
+</html>',
+
+'<!DOCTYPE html>\
+<head>\
+  <link rel="stylesheet" href="../characters.css">\
+  <link rel="icon" href="../icons/lustwreaker.png">\
+</head>\
+<title>Rory Lustwreaker</title>\
+<html>\
+  <body>\
+    <h1 style="color: #980000;"><span style="color: #cc0000;">Rory</span> Lustwreaker</h1>\
+    <p>\
+    She/Her<br>\
+    Female<br>\
+    old enough<br>\
+    Bi with b female preference<br>\
+    Fists/melee<br>\
+    Greater Succubus<br><br>\
+    Short in height, small chest, keeps her horns, tail and wings hidden, very submissive, isn\'t affected by semen the way succubi normally are, likes women contrary to her evolutionary design<br><br>Bound herself to Marnin out of love, and was named\
+    </p>\
+  </body>\
+</html>',
+
+'<!DOCTYPE html>\
+<head>\
+  <link rel="stylesheet" href="../characters.css">\
+  <link rel="icon" href="../icons/lustwreaker.png">\
+</head>\
+<title>Diolas Lustwreaker</title>\
+<html>\
+  <body>\
+    <h1 style="color: #980000;"><span style="color: #783f04;">Diolas</span> Lustwreaker</h1>\
+    <p>\
+    They/Them<br>\
+    Male<br>\
+    old enough<br>\
+    Doesn\'t know other than they like Raein<br>\
+    Angelic power<br>\
+    Demon/Angel(Nephilim)<br><br>\
+    Forced away from Heaven and Hell, in a kind of exile on Earth<br>\
+    </p>\
+  </body>\
+</html>',
+
+'<!DOCTYPE html>\
+<head>\
+  <link rel="stylesheet" href="../characters.css">\
+  <link rel="icon" href="../icons/lustwreaker.png">\
+</head>\
+<title>Raein Lustwreaker</title>\
+<html>\
+  <body>\
+    <h1 style="color: #980000;"><span style="color: #bf9000;">Raein </span>Lustwreaker</h1>\
+    <p>\
+    He/Him<br>\
+    Male<br>\
+    old enough<br>\
+    Gay<br>\
+    Holy spear<br>\
+    Fallen Angel<br><br>\
+    Rory threw him from grace after he attacked her, Marnin and Diolas<br>\
+    </p>\
+  </body>\
+</html>',
+
+'<!DOCTYPE html>\
+<head>\
+  <link rel="stylesheet" href="../characters.css">\
+  <link rel="icon" href="../icons/lustwreaker.png">\
+</head>\
+<title>Quinn Lustwreaker</title>\
+<html>\
+  <body>\
+    <h1 style="color: #980000;"><span style="color: #660000;">Quinn(Quilin)</span> Lustwreaker</h1>\
+    <p>\
+    She/Her<br>\
+    Female<br>\
+    equivalent of 16<br>\
+    don\'t know yet<br>\
+    Katana<br>\
+    Demonoid - quarter human, 3/4 succubus<br><br>\
+    Daughter of Marnin and Rory<br>weeb perhaps? at least very interested in japanese culture\
+    </p>\
+  </body>\
+</html>']
+
+// const test = fetchJSONData()
--- test server.sh
+++ test server.sh
@@ -1,2 +1,2 @@
 cd /home/benheroblaw/Documents/GitHub/prokid99999.github.io
-python3 -m http.server
+python3 -m http.server 2009
